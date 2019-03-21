7940585d04688f91ebf879bcb2e26cf679dace40
Joris Laurenssen <jlaurenssen@frencken.nl>
2019-03-21 08:28:59 +0100

    added if/else snippet build script works

--- scripts/ACS_Load_PRG.ps1
+++ scripts/ACS_Load_PRG.ps1
@@ -1,13 +1,14 @@
 param([string]$ip="10.0.0.100")
 $file=$args[0]
 
-$file
-$ip
-
 $Ch = New-Object -ComObject SpiiPlusCOM660.Channel
 $Ch.OpenCommEthernetTCP($ip, $Ch.ACSC_SOCKET_STREAM_PORT)
-# $Ch.StopBuffer($Ch.ACSC_NONE)
 $Ch.Transaction("##SR")
 $Ch.LoadBuffersFromFile($file)
 $Ch.CompileBuffer($Ch.ACSC_NONE)
+for ($i=0; $i -le 100; $i++) {
+    try{
+        $Ch.RunBuffer($i, "AUTOEXEC")
+    } catch {}
+}
 $Ch.CloseComm()
